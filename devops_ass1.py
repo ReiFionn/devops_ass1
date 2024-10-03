e2ec24cc18eb2955f9fc4e2c1df810f07b87301d
reifionn <20101977@mail.wit.ie>
2024-10-03 18:48:19 +0100

    Cleaning

--- devops_ass1.py
+++ devops_ass1.py
@@ -2,19 +2,19 @@
 import boto3, json, random, string, subprocess
 
 ec2 = boto3.resource('ec2')
-s3 = boto3.resource("s3")
-s3client = boto3.client("s3")
+s3 = boto3.resource('s3')
+s3client = boto3.client('s3')
 key_name = 'EvaUnit00'
 security_group = 'sg-0d2f8f0a77bd98152'
 image_url = 'http://devops.witdemo.net/logo.jpg'
 
-print(f"!!! Terminating previous instances (if any)...")
+print(f'!!! Terminating previous instances (if any)...')
 for instance in ec2.instances.all():
     if instance.state['Name'] == 'running':
         response = instance.terminate()
         print(response)
 
-print("!!! Emptying previous buckets (if any)...")
+print('!!! Emptying previous buckets (if any)...')
 for bucket in s3.buckets.all():
     for key in bucket.objects.all():
         try:
@@ -23,7 +23,7 @@ for bucket in s3.buckets.all():
         except Exception as error:
             print (error)
 
-print("!!! Deleteing previous buckets (if any)...")
+print('!!! Deleteing previous buckets (if any)...')
 for bucket in s3.buckets.all():
     try:
         response = bucket.delete()
@@ -31,7 +31,7 @@ for bucket in s3.buckets.all():
     except Exception as error:
         print (error)
 
-print("!!! Preparing new EC2 instance...")
+print('!!! Preparing new EC2 instance...')
 
 new_instances = ec2.create_instances(
     ImageId='ami-0ebfd941bbafe70c6',
@@ -65,7 +65,7 @@ curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-da
 
 cp index.html /var/www/html/index.html""")
 
-print("!!! Preparing new EC2 instance's tags...")
+print('!!! Preparing new EC2 instancet\'s tags...')
 
 new_instances[0].reload()
 new_instances[0].create_tags(
@@ -85,12 +85,12 @@ print (f'!!! Instance {new_instances[0].id} is running! Visit the web page at: h
 
 #https://www.pythontutorial.net/python-basics/python-write-text-file/
 with open('freilly-websites.txt','w') as file:
-    file.write(f"EC2 Instance: http://{new_instances[0].public_ip_address}")
+    file.write(f'EC2 Instance: http://{new_instances[0].public_ip_address}')
 
 print('!!! Creating S3 bucket...')
 
 #https://stackoverflow.com/questions/2030053/how-to-generate-random-strings-in-python
-new_bucket = s3.create_bucket(Bucket=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))+"-freilly")
+new_bucket = s3.create_bucket(Bucket=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))+'-freilly')
 
 print(f'!!! Configuring {new_bucket.name} website...')
 
@@ -119,14 +119,14 @@ bucket_policy = {
 
 s3.Bucket(new_bucket.name).Policy().put(Policy=json.dumps(bucket_policy))
 
-print(f"!!! Downloading image from {image_url}...")
+print(f'!!! Downloading image from {image_url}...')
 try:
     response = subprocess.run(['curl','-O', image_url])
     print(response)
 except Exception as error:
     print (error)
 
-print(f"!!! Uploading image from {image_url} to {new_bucket.name}...")
+print(f'!!! Uploading image from {image_url} to {new_bucket.name}...')
 try:
     response = s3.Object(new_bucket.name,'logo.jpg').put(Body=open('logo.jpg','rb'),ContentType='image/jpeg')
     print(response)
@@ -148,13 +148,15 @@ with open('index.html','w') as file:
     </body>
 </html>""")
     
-print(f"!!! Uploading index.html to {new_bucket.name}...")
+print(f'!!! Uploading index.html to {new_bucket.name}...')
 try:
     response = s3.Object(new_bucket.name,'index.html').put(Body=open('index.html','rb'),ContentType='text/html')
     print(response)
 except Exception as error:
  print (error)
 
-print(f"!!! {new_bucket.name} launched! Visit the web page at: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com\n!!! Writing website to freilly-websites.txt...")
+print(f'!!! {new_bucket.name} launched! Visit the web page at: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com\n!!! Writing website to freilly-websites.txt...')
 with open('freilly-websites.txt','a') as file:
-    file.write(f"\nS3 Bucket: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com")
+    file.write(f'\nS3 Bucket: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com')
+
+print('!!! END OF SCRIPT !!!')
