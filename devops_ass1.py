130477bf53f95e7e56c2d420cfca94120f028a79
reifionn <20101977@mail.wit.ie>
2024-10-10 17:15:29 +0100

    added logging method, changed error prints, ensuring key has correct permissions, waits for EC2 web service to run before running monitoring.sh, runs monitoring.sh, added extra functionality to monitoring.sh, general cleaning

--- devops_ass1.py
+++ devops_ass1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import boto3, json, random, string, subprocess, datetime
+import boto3, json, random, string, subprocess, datetime, time
 
 ec2 = boto3.resource('ec2')
 s3 = boto3.resource('s3')
@@ -8,44 +8,48 @@ key_name = 'EvaUnit00'
 security_group = 'sg-0d2f8f0a77bd98152'
 image_url = 'http://devops.witdemo.net/logo.jpg'
 
-print('!!! Creating log file...')
+print('!!! START OF SCRIPT !!!\n!!! Creating log file...')
 #https://www.pythontutorial.net/python-basics/python-write-text-file/
 try: 
     with open('logs.txt','w') as file:
         file.write('//////////////////// START OF LOGS ////////////////////\n')
 except Exception as error:
-    print(error)
+    print(f'Error while creating log file: {error}')
+
+print('!!! Defining error logging method...')
+def log_to_logs(log):
+    try:
+        with open('logs.txt', 'a') as file:
+            #https://www.w3schools.com/python/python_datetime.asp
+            file.write(f'{datetime.datetime.now()} : {log}\n')
+    except Exception as error:
+        print(f'Error writing to log file: {error}')
 
 print('!!! Terminating previous instances (if any)...')
 for instance in ec2.instances.all():
     if instance.state['Name'] == 'running':
         try:
             response = instance.terminate()
-            try:
-                with open('logs.txt','a') as file:
-                    #https://www.w3schools.com/python/python_datetime.asp
-                    file.write(f'{datetime.datetime.now()} : {response}')
-            except Exception as error:
-                print(error)
+            log_to_logs(response)
         except Exception as error:
-            print (error)
+            print (f'Error while terminating previous instances: {error}')
 
 print('!!! Emptying previous buckets (if any)...')
 for bucket in s3.buckets.all():
     for key in bucket.objects.all():
         try:
             response = key.delete()
-            #print (response)
+            log_to_logs(response)
         except Exception as error:
-            print (error)
+            print (f'Error while emptying previous buckets: {error}')
 
 print('!!! Deleteing previous buckets (if any)...')
 for bucket in s3.buckets.all():
     try:
         response = bucket.delete()
-        #print(response)
+        log_to_logs(response)
     except Exception as error:
-        print (error)
+        print (f'Error while deleting previous buckets: {error}')
 
 print('!!! Preparing new EC2 instance...')
 try:
@@ -55,7 +59,7 @@ try:
     MaxCount=1,
     InstanceType='t2.nano',
     KeyName=key_name,
-    SecurityGroupIds=[security_group],
+    SecurityGroupIds=[security_group],#TODO ADD NEW AWS FUNCTION?
     UserData=f"""#!/bin/bash
                 yum install httpd -y
                 systemctl start httpd
@@ -80,13 +84,14 @@ try:
                 curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/security-groups >> index.html
 
                 cp index.html /var/www/html/index.html""")
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error creating EC2 instance: {error}\n!!! Stopping script...')
+    exit()
 
-print('!!! Preparing new EC2 instancet\'s tags...')
-
+print('!!! Preparing new EC2 instance\'s tags...')
 try:
-    new_instances[0].create_tags(
+    response = new_instances[0].create_tags(
         Tags=[
             {
                 'Key': 'Name',
@@ -94,27 +99,30 @@ try:
             },
         ]
     )
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error while creating new instance\'s tags: {error}')
 
-new_instances[0].reload()
-print (f'!!! Instance {new_instances[0].id} created sucessfully!\n!!! Waiting for instance to run...')
+new_instances[0].wait_until_exists()
+print (f'!!! Instance {new_instances[0].id} created successfully!\n!!! Waiting for instance to run...')
 new_instances[0].wait_until_running()
-new_instances[0].reload()
 print (f'!!! Instance {new_instances[0].id} is running! Visit the web page at: http://{new_instances[0].public_ip_address}\n!!! Writing website to freilly-websites.txt...')
 
 try: 
     with open('freilly-websites.txt','w') as file:
-        file.write(f'EC2 Instance: http://{new_instances[0].public_ip_address}')
+        response = file.write(f'EC2 Instance: http://{new_instances[0].public_ip_address}')
+    log_to_logs(response)        
 except Exception as error:
-    print(error)
+    print(f'Error writing to website file: {error}')
 
 print('!!! Creating S3 bucket...')
 #https://stackoverflow.com/questions/2030053/how-to-generate-random-strings-in-python
 try:
     new_bucket = s3.create_bucket(Bucket=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))+'-freilly')
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error creating S3 bucket: {error}\n!!! Stopping script...')
+    exit()
 
 print(f'!!! Configuring {new_bucket.name} website...')
 website_configuration = {
@@ -123,14 +131,17 @@ website_configuration = {
 }
 
 try:
-    s3.BucketWebsite(new_bucket.name).put(WebsiteConfiguration=website_configuration)
+    response = s3.BucketWebsite(new_bucket.name).put(WebsiteConfiguration=website_configuration)
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error putting website configuration into S3 bucket: {error}\n!!! Stopping script...')
+    exit()
 
 try:
-    s3client.delete_public_access_block(Bucket=new_bucket.name)
+    response = s3client.delete_public_access_block(Bucket=new_bucket.name)
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error deleting public access block for S3 bucket: {error}')
 
 bucket_policy = {
     "Version": "2012-10-17",
@@ -147,73 +158,109 @@ bucket_policy = {
 }
 
 try:
-    s3.Bucket(new_bucket.name).Policy().put(Policy=json.dumps(bucket_policy))
+    response = s3.Bucket(new_bucket.name).Policy().put(Policy=json.dumps(bucket_policy))
+    log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error putting new policy into S3 bucket: {error}')
 
 print(f'!!! Downloading image from {image_url}...')
 try:
-    response = subprocess.run(['curl','-O', image_url])
-    #print(response)
+    response = subprocess.run(['curl','-O', image_url], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    log_to_logs(response)
 except Exception as error:
-    print (error)
+    print (f'Error downloading image from {image_url}: {error}')
 
 print(f'!!! Uploading image from {image_url} to {new_bucket.name}...')
 try:
     response = s3.Object(new_bucket.name,'logo.jpg').put(Body=open('logo.jpg','rb'),ContentType='image/jpeg')
-    #print(response)
+    log_to_logs(response)
 except Exception as error:
-    print (error)
+    print (f'Error uploading image from {image_url} to {new_bucket.name}: {error}')
 
 print("!!! Creating index.html...")
 
 #https://www.geeksforgeeks.org/creating-and-viewing-html-files-with-python/
 try:
     with open('index.html','w') as file:
-        file.write(f"""<html>
-        <head>
-            <title>{new_bucket.name}'s Website!</title>
-        </head>
-        <body>
-            <h2>Here is the image from {image_url}!</h2>
-            <img src="http://{new_bucket.name}.s3.amazonaws.com/logo.jpg" alt="Looks like it didn't work LOL!">
-            <p>Isn't it great! :D</p>
-        </body>
-        </html>""")
+        response = file.write(f"""<html>
+    <head>
+        <title>{new_bucket.name}'s Website!</title>
+    </head>
+    <body>
+        <h2>Here is the image from {image_url}!</h2>
+        <img src="http://{new_bucket.name}.s3.amazonaws.com/logo.jpg" alt="Looks like it didn't work LOL!">
+        <p>Isn't it great! :D</p>
+    </body>
+</html>""")
+        log_to_logs(response)
 except Exception as error:
-    print(error)
+    print(f'Error creating index.html file: {error}')
     
 print(f'!!! Uploading index.html to {new_bucket.name}...')
 try:
     response = s3.Object(new_bucket.name,'index.html').put(Body=open('index.html','rb'),ContentType='text/html')
-    #print(response)
+    log_to_logs(response)
 except Exception as error:
- print (error)
+ print (f'Error uploading index.html to {new_bucket.name}: {error}')
 
 print(f'!!! {new_bucket.name} launched! Visit the web page at: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com\n!!! Writing website to freilly-websites.txt...')
-with open('freilly-websites.txt','a') as file:
-    file.write(f'\nS3 Bucket: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com')
+try:
+    with open('freilly-websites.txt','a') as file:
+        response = file.write(f'\nS3 Bucket: http://{new_bucket.name}.s3-website-us-east-1.amazonaws.com')
+    log_to_logs(response)
+except Exception as error:
+    print(f'Error writing to website file: {error}')
+
+print(f'!!! Ensuring {key_name}.pem has correct permissions...')
+try:
+    command = f"chmod 400 {key_name}.pem"
+    #https://stackoverflow.com/questions/11269575/how-to-hide-output-of-subprocess
+    response = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    log_to_logs(response)
+except Exception as error:
+    print(f'Error ensuring {key_name}.pem has correct permissions: {error}')
 
 print(f'!!! Copying monitoring.sh to {new_instances[0].public_ip_address}...')
 try:
     command = f"scp -o StrictHostKeyChecking=no -i {key_name}.pem monitoring.sh ec2-user@{new_instances[0].public_ip_address}:."
-    response = subprocess.run(command, shell=True)
+    response = subprocess.run(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    log_to_logs(response)
 except Exception as error:
-    print (error)
+    print (f'Error copying monitoring.sh to {new_instances[0].public_ip_address}: {error}')
 
 print('!!! Making monitoring.sh executable...')
 try:
     command = f"ssh -i {key_name}.pem ec2-user@{new_instances[0].public_ip_address} 'chmod 700 monitoring.sh'"
     response = subprocess.run(command,shell=True)
+    log_to_logs(response)
 except Exception as error:
-    print (error)
+    print (f'Error making monitoring.sh executable: {error}\n!!! Stopping script...')
+    exit()
+
+print('!!! Waiting for EC2 web service to run...')
+#https://www.tecmint.com/check-apache-httpd-status-and-uptime-in-linux/
+while True:
+    try:
+       command = f"ssh -i {key_name}.pem ec2-user@{new_instances[0].public_ip_address} 'systemctl is-active httpd'"
+       response = subprocess.run(command, shell=True, capture_output=True, text=True)
+       log_to_logs(response)
+       if "inactive" not in response.stdout: break
+       time.sleep(3)
+    except Exception as error:
+        print(f'Error checking if httpd service is running: {error}')
 
 print('!!! Running monitoring.sh...')
 try:
     command = f"ssh -i {key_name}.pem ec2-user@{new_instances[0].public_ip_address} './monitoring.sh'"
-    response = subprocess.run(command,shell=True,capture_output=True,text=True)
-    print(response.stdout.strip())
+    response = subprocess.run(command,shell=True)
+    log_to_logs(response)
 except Exception as error:
-    print (error)
+    print (f'Error running monitoring.sh: {error}')
+
+try: 
+    with open('logs.txt','a') as file:
+        file.write('//////////////////// END OF LOGS ////////////////////\n')
+except Exception as error:
+    print(f'Error writing to log file: {error}')
 
 print('!!! END OF SCRIPT !!!')
--- monitoring.sh
+++ monitoring.sh
@@ -6,13 +6,20 @@ INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 MEMORYUSAGE=$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')
 PROCESSES=$(expr $(ps -A | grep -c .) - 1)
 HTTPD_PROCESSES=$(ps -A | grep -c httpd)
+#https://geeksforgeeks.org/linux-system-monitoring-commands-and-tools/
+DISK_PERFORMANCE=$(iostat -dx | awk 'NR==4 {print $NF"%"}') #gets I/O statistics, selecting the 4th line, grabs the last field which is the percentage of time the disc is doing operations
+NETSTAT_CONNECTIONS=$(netstat -an | grep -c ESTABLISHED) #lists all connections in numberic format, counts all connections in the ESTABLISHED state
 
+echo "//////////////////// START OF MONITORING.SH ////////////////////"
 echo "Instance ID: $INSTANCE_ID"
 echo "Memory utilisation: $MEMORYUSAGE"
 echo "No of processes: $PROCESSES"
+echo "Disk performace (utilization): $DISK_PERFORMANCE"
+echo "Established network connections: $NETSTAT_CONNECTIONS"
 if [ $HTTPD_PROCESSES -ge 1 ]
 then
     echo "Web server is running"
 else
     echo "Web server is NOT running"
 fi
+echo "//////////////////// END OF MONITORING.SH ////////////////////"
